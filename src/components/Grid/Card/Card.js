ddf70e35f50ac80a11a3a7bf90e1a57011a9c5d1
marco981 <marco98x2@gmail.com>
2019-04-09 21:05:20 +0100

    style card

--- src/components/Grid/Card/Card.js
+++ src/components/Grid/Card/Card.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import styles from './Card.module.css'
 
-const card = props => (
-  <div>
-    {props.title}
-    <img src={props.poster} alt={props.title} />
-  </div>
-)
+const card = props => {
+  return (
+    <div className={styles.Card}>
+      <div className={styles.Card__imgContainer}>
+        <img
+          className={styles.Card__img}
+          src={props.poster}
+          alt={props.title}
+        />
+      </div>
+      <div className={styles.Card__description}>
+        <h2>
+          {props.title} {props.year}
+        </h2>
+      </div>
+    </div>
+  )
+}
 
 export default card
