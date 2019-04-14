40b96d4590414ab476e59fcaad41bca6d2b35bc2
marco981 <marco98x2@gmail.com>
2019-04-14 18:03:31 +0100

    add TopBar

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './TopBar.module.css'
+
+const topBar = props => {
+  return (
+    <div className={styles.TopBar}>
+      <h1>FindThatMovie</h1>
+    </div>
+  )
+}
+
+export default topBar
