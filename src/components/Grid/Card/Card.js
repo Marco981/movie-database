d9c85c4cf6ea2d5863a508802c94f44194c2878c
marco981 <marco98x2@gmail.com>
2019-05-01 21:10:16 +0100

    create hover effect on cards

--- src/components/Grid/Card/Card.js
+++ src/components/Grid/Card/Card.js
@@ -2,39 +2,26 @@ import React from 'react'
 import styles from './Card.module.css'
 
 const card = props => {
-  const over = e => {
-    console.log(e.target.closest('.Card'))
-
-    // e.target.closest('Card').classList.add('hover')
-  }
-
-  const out = e => {
-    // e.target.closest('Card').classList.remove('hover')
-  }
-
   const url = `https://www.imdb.com/title/${props.imdbId}`
 
   return (
-    <div
-      data-name='class'
-      className={styles.Card}
-      onMouseOver={over}
-      onMouseOut={out}
-    >
-      <div className={styles.Card__imgContainer}>
-        <img
-          className={styles.Card__img}
-          src={props.poster}
-          alt={props.title}
-        />
-        <a className={styles.Button} href={url} taget='_blank'>
-          IMDB
-        </a>
+    <div className={styles.Card}>
+      <div className={styles.Card__front}>
+        <div className={styles.Card__imgContainer}>
+          <img
+            className={styles.Card__img}
+            src={props.poster}
+            alt={props.title}
+          />
+        </div>
+        <div className={styles.Card__titleContainer}>
+          <h2>
+            {props.title} {props.year}
+          </h2>
+        </div>
       </div>
-      <div className={styles.Card__description}>
-        <h2>
-          {props.title} {props.year}
-        </h2>
+      <div className={styles.Card__back}>
+        <p>Movie description</p>
       </div>
     </div>
   )
