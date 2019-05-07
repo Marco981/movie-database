ce1780feb95940a3f02014854f6bc1f8ac307316
marco981 <marco98x2@gmail.com>
2019-05-07 20:32:47 +0100

    change variable names and prepare for routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import styles from './App.module.css'
 import Form from './components/Form/Form'
 import Grid from './components/Grid/Grid'
 import axios from 'axios'
 import TopBar from './components/TopBar/TopBar'
+import MovieDetails from './components/MovieDetails/MovieDetails'
 import Pagination from './components/Pagination/Pagination'
 
 // axios.defaults.baseURL =
@@ -25,15 +27,15 @@ class App extends Component {
     })
   }
 
-  findMoviesHandler = (movie, pageId) => {
-    const query = pageId ? this.state.savedSearch : this.state.inputValue
+  findMoviesHandler = (movie, pageNumber) => {
+    const query = pageNumber ? this.state.savedSearch : this.state.inputValue
     axios
       .get(
         'https://api.themoviedb.org/3/search/movie?api_key=e16c1f5204dd23fdebe3cf4ed1ca9581',
         {
           params: {
             query: query.toLowerCase(),
-            page: pageId || null
+            page: pageNumber || null
           }
         }
       )
@@ -63,28 +65,40 @@ class App extends Component {
       })
   }
 
-  changePageHandler = id => {
+  changePageHandler = pageNumber => {
     const movie = this.state.savedSearch
-    this.findMoviesHandler(movie, id)
+    this.findMoviesHandler(movie, pageNumber)
   }
 
   render () {
     return (
-      <div className={styles.App}>
-        <TopBar />
-        <Form
-          value={this.state.inputValue}
-          changed={e => this.inputChangedHandler(e)}
-          clicked={this.findMoviesHandler}
-        />
-        <Grid error={this.state.error} movies={this.state.moviesList} />
-        <Pagination
-          error={this.state.error}
-          page={this.state.page}
-          clicked={e => this.changePageHandler(e.target.innerText)}
-          numPages={this.state.totalPages}
-        />
-      </div>
+      <Router>
+        <div className={styles.App}>
+          <TopBar />
+          {/* <Switch> */}
+          {/* <Route path='/' exact component={Form} />
+          <Route path='/' exact component={Grid} />
+          <Route path='/' exact component={Pagination} />
+          <Route path='/:id' exact component={MovieDetails} /> */}
+          {/* </Switch> */}
+          <p className={styles.Subtitle}>
+            Honestly, the best place on the internet to find informations about
+            your favourite movies
+          </p>
+          <Form
+            value={this.state.inputValue}
+            changed={e => this.inputChangedHandler(e)}
+            clicked={this.findMoviesHandler}
+          />
+          <Grid error={this.state.error} movies={this.state.moviesList} />
+          <Pagination
+            error={this.state.error}
+            page={this.state.page}
+            clicked={e => this.changePageHandler(e.target.innerText)}
+            numPages={this.state.totalPages}
+          />
+        </div>
+      </Router>
     )
   }
 }
